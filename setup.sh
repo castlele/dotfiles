14c8504e85bee9474807ab712751940ef45e1c51
castlele <castlelecs@gmail.com>
2024-05-01 19:56:30 +0300

    update configuration for linux

--- setup.sh
+++ setup.sh
@@ -5,31 +5,21 @@ nvim_submodule_dir="/nvim"
 nvim_config_dir=$config_dir$nvim_submodule_dir
 tmux_submodule_dir="/tmux"
 tmux_config_dir=$config_dir$tmux_submodule_dir
+alacritty_dir="/alacritty"
+alacritty_config_dir=$config_dir$alacritty_dir
 pwd=$(pwd)
 
-create_directories()
-{
-    if [ ! -d $config_dir ]; then
-        echo "Creating .config directory..."
-        mkdir $config_dir
-    fi
-}
-
-clone_projects()
-{
-    if [ ! -d "."$nvim_submodule_dir ]; then
-        git clone git@github.com:castlele/nvim.git
-    fi
-
-    if [ ! -d "."$tmux_submodule_dir ]; then
-        git clone git@github.com:castlele/tmux.git
-    fi
-
+clone_projects() {
     git submodule update --init --recursive
 }
 
-symlink_dotfiles()
-{
+symlink_dotfiles() {
+    if [ ! -d $alacritty_config_dir ]; then
+        echo "Creating symlink for alacritty config"
+        ln -s $pwd$alacritty_dir $alacritty_config_dir
+        cp alacritty.toml ~/.alacritty.toml
+    fi
+
     if [ ! -d $nvim_config_dir ]; then
         echo "Creating symlink for nvim config"
         ln -s $pwd$nvim_submodule_dir $nvim_config_dir
@@ -41,8 +31,7 @@ symlink_dotfiles()
     fi
 }
 
-setup_dotfiles()
-{
+setup_dotfiles() {
     echo "Configuration of nvim config"
     cd $nvim_config_dir
     ./setup.sh
@@ -52,7 +41,6 @@ setup_dotfiles()
     ./setup.sh
 }
 
-create_directories
 clone_projects
 symlink_dotfiles
 setup_dotfiles
--- tmux/setup.sh
+++ tmux/setup.sh
@@ -2,3 +2,12 @@
 
 # git clone https://github.com/tmux-plugins/tpm
 
+OS_TYPE="$OSTYPE"
+LINUX_MINT="linux-gnu"
+MACOS="darwin"
+
+if [[ $OS_TYPE == $LINUX_MINT* ]]; then
+    sudo apt install tmux
+elif [[ $OS_TYPE == $MACOS* ]]; then
+    brew install tmux
+fi
