6b7ba1f78b7875a24aa808eb49565a47f6e9c453
castlele <castlelecs@gmail.com>
2024-07-06 17:34:10 +0300

    fix: printing public key

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ setupGit() {
     ssh-keygen -t ed25519 -C "$EMAIL"
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_ed25519
-    cat ~/.ssh/id_ed25519
+    cat ~/.ssh/id_ed25519.pub
 
     cp gitconfig ~/.gitconfig
 }
