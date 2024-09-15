fab9247aa2431ff8a278d5b0ddeb84aa96c5ef44
castlele <castlelecs@gmail.com>
2024-09-15 13:24:48 +0300

    add cargo installation

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ cloneProjects() {
 setupNeovim() {
     echo "Setup Neovim"
 
+    curl https://sh.rustup.rs -sSf | sh
     cargo install tree-sitter-cli
 
     ln -s $PWD/nvim/ $CONFIG_DIR/nvim
@@ -59,10 +60,12 @@ setupTmux() {
 setupLazygit() {
     echo "Setup Lazygit"
 
-    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-    tar xf lazygit.tar.gz lazygit
-    sudo install lazygit /usr/local/bin
+    if [[ $OS_TYPE == $LINUX_MINT* ]]; then
+        LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+        curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+        tar xf lazygit.tar.gz lazygit
+        sudo install lazygit /usr/local/bin
+    fi
 }
 
 setupKitty() {
